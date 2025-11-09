25c52ac4ba798c615496a8b47f28a073911afe28
Chanakarn Kingkaew <chanakarnkin@Chanakarns-MacBook-Pro.local>
2025-11-09 10:02:32 +0700

    keyevent

--- 7.2 - Drumkit/index.js
+++ 7.2 - Drumkit/index.js
@@ -59,3 +59,38 @@ document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 // document.querySelectorAll(".drum").forEach(b => {
 //     b.addEventListener("click", ()=> alert('ฉันถูกกด'))
 // })
+
+
+document.addEventListener("keydown", function (event) {
+    console.log(event.key)
+
+    if (event.key == 'w') {
+         var audio = new Audio('sounds/tom-1.mp3')
+        audio.play();
+    }
+     if (event.key == 'a') {
+         var audio = new Audio('sounds/tom-2.mp3')
+        audio.play();
+    }
+     if (event.key == 's') {
+         var audio = new Audio('sounds/tom-3.mp3')
+        audio.play();
+    }
+     if (event.key == 'd') {
+         var audio = new Audio('sounds/tom-4.mp3')
+        audio.play();
+    }
+     if (event.key == 'j') {
+         var audio = new Audio('sounds/crash.mp3')
+        audio.play();
+    }
+     if (event.key == 'k') {
+         var audio = new Audio('sounds/kick-bass.mp3')
+        audio.play();
+    }
+     if (event.key == 'l') {
+         var audio = new Audio('sounds/snare.mp3')
+        audio.play();
+    }
+
+})
